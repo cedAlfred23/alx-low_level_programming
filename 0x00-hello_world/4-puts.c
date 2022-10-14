a6d371b5a3e87d5ffce980a6d116d5d969d3d5ae
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-14 10:42:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /*
- *"Programming is like building a multilingual puzzle
+ * main- Serves as the entry point for the program
+ *
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
  */
 int main(void)
 {
