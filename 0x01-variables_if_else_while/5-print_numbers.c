be0e9417d26c3fad929466e0770efe45853c862e
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-16 16:39:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		print(d);
+	print('\n');
+	return (0);
+}
