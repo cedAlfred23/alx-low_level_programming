4d1d3fb5c43e5ec7b1d73c92dd7335892f929e5f
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-16 16:25:05 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>;
+#include <stdio.h>;
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c)
+	putchar('\n');
+	return (0);
+}
