f8a52a965f29429b4f3aa73964fc653e5fb917d8
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-16 17:08:40 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	int d;
 
-        for (d = 0; d < 10; d++)
-                putchar(d + '0');
-        putchar('\n');
-        return (0);
+	for (d = 0; d < 10; d++)
+		putchar(d + '0');
+	putchar('\n');
+	return (0);
 }
