94acbccbe91fe8bfade858a16683170b12af6282
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-18 12:54:37 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 /*
  *main checks for lowercase character
  *Return: 0 on success
  */
 int _islower(int c)
 {
-	is_lower(c) ? return (1) : return (0);
+	return (c >= 'a' && c <= 'z');
 }
