b17cab362f2bb7f9140fb77454db645b807ce9c2
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-18 06:03:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char str[] = "_putchar";
 	
-	for (int i = 0 ; i < strlen(len) ; i++)
-		putchar(str[]);
+	for (int i = 0 ; i < strlen(str) ; i++)
+		putchar(str[i]);
 	return 0;
 }
