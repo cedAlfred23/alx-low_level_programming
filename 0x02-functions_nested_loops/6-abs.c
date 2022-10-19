61edd071739a8fd232c7cad674634a045d10665a
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-18 20:55:32 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @r: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int r)
+{
+	return (abs(r));
+}
