02d5de97794612b3503bb681acdedcf488e9efd0
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-20 21:18:19 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,20 +7,23 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
-	
-	//The value of i is always smaller than 10
+	i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
+	/**
+	 *The value of i is always smaller than 10
+	 * this causes infinite loop
+	 */
 
-        printf("Infinite loop avoided! \\o/\n");
+	while (i < 10)
+	{
+		putchar(i);
+	}
 
-        return (0);
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
