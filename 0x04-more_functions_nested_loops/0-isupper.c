4392246cd7a3ad6d6848cdf76e531aef5867b286
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-20 22:05:44 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: is the charater to check
+ * Return: 0 on success
+ */
+int _isupper(int c)
+{
+	isupper(c) ? return (1) : return (0);
+}
