c1b391cf38b99e517252285a60fbe5ec6af508b9
“Cedric <al.cedricgbaguidi@gmail.com>
2022-10-24 22:15:45 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: pointer to reset to 0
+ *
+ * Returns: 0 on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
